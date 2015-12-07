725f59d6a038012659c57c345060c413431d2cca
CBA_Santo <santo@cybridge.jp>
2015-12-07 18:40:48 +0900

    commit

--- application/views/_includes/navbar.php
+++ application/views/_includes/navbar.php
@@ -23,6 +23,7 @@
 			<button type="submit" class="btn btn-default">Submit</button>
 		</form>
 		<ul class="nav navbar-nav navbar-right">
+			<li><a href="<?= base_url(); ?>calendar">Lịch gia đình</a></li>
 			<li><a href="<?= base_url(); ?>count_down">Count down</a></li>
 			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Người dùng <b class="caret"></b></a>
--- application/views/calendar.php
+++ application/views/calendar.php
@@ -1,3 +1,4 @@
 <?php $this->load->view('_includes/header',["css"=>[base_url()."asset/css/login.css"]]); ?>
+<h2>Lịch gia đình</h2>
 <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=atpk1cqfhu1daimocnnosmv9eo%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=Asia%2FSaigon" style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
 <?php $this->load->view('_includes/footer'); ?>
