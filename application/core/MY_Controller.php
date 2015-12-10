e4f6bff13fe18158a9cb21a520afe65cba750eb5
CBA_Santo <santo@cybridge.jp>
2015-12-10 20:26:20 +0900

    commit

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -7,6 +7,9 @@ class MY_Controller extends CI_Controller
 	public function __construct(){
 		parent::__construct();
 		if($this->router->fetch_method() != "login") {
+			if($this->router->fetch_method()=="cron"){
+				return;
+			}
 			if(!$this->session->userdata('user')){
 				redirect('homepage/login','refresh');
 			}
