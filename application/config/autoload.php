bf189c81fa6200c0079e75a9ff765dbb6de7aede
CBA_Santo <santo@cybridge.jp>
2015-12-18 13:35:11 +0900

    commit

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+require FCPATH."vendor/autoload.php";
 /*
 | -------------------------------------------------------------------
 | AUTO-LOADER
