a11544f1a7fa6ec756f0e833ce45a6909f70f31c
CBA_Santo <santo@cybridge.jp>
2015-12-22 13:24:49 +0900

    commit

--- application/models/Files_model.php
+++ application/models/Files_model.php
@@ -46,24 +46,11 @@ class Files_model extends CI_Model {
 
 	public function install_db(){
 
-
 		if($this->db->query("SELECT name FROM sqlite_master WHERE name='files'")->num_rows()==0){
 		$sql="
-----
--- phpLiteAdmin database dump (https://bitbucket.org/phpliteadmin/public)
--- phpLiteAdmin version: 1.9.6
--- Exported: 12:21pm on December 22, 2015 (JST)
--- database file: application/models/db/family
-----
-BEGIN TRANSACTION;
+			CREATE TABLE 'files' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'files_name' TEXT NOT NULL, 'files_path' TEXT, 'files_size' REAL, 'files_type' TEXT, 'delete_flg' INTEGER NOT NULL DEFAULT 0 , 'files_title' TEXT);
+		";
 
-----
--- Table structure for files
-----
-CREATE TABLE 'files' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'files_name' TEXT NOT NULL, 'files_path' TEXT, 'files_size' REAL, 'files_type' TEXT, 'delete_flg' INTEGER NOT NULL DEFAULT 0 );
-COMMIT;
-
-";
 			$this->db->query($sql);
 		}
 	}
