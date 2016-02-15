41f798b8af08535d5f07bfbc4ca26d7db9803fc6
taro.yamada <taro.yamada@example.com>
2016-02-15 11:09:35 +0900

    Fix link of custom tool

--- application/views/custom/tool.php
+++ application/views/custom/tool.php
@@ -15,10 +15,7 @@
 
 			</a>
 			<a class='btn btn-lg btn-info' href="https://www.facebook.com/conduonghanhphuc/timeline" target="_blank">
-				<i class="fa fa-facebook">
-
-				</i>acebook: Timeline
-
+				<i class="fa fa-facebook"></i>acebook: Timeline
 			</a>
 
 		</p>
@@ -28,37 +25,22 @@
 	<div class="col-md-3 text-center">
 		<p>
 			<a href="https://www.facebook.com/conduonghanhphuc/photos_stream" target="_blank" class="thumbnail">
-
 				<img src="/asset/data/photo.jpg">
-
-
 			</a>
 			<a class='btn btn-lg btn-info' href="https://www.facebook.com/conduonghanhphuc/photos_stream" target="_blank">
-				<i class="fa fa-facebook">
-
-				</i>acebook: Photos_stream
-
+				<i class="fa fa-facebook"></i>acebook: Photos_stream
 			</a>
-
 		</p>
-
 	</div>
-
 	<div class="col-md-3 text-center">
 		<p>
 			<a href="https://www.facebook.com/conduonghanhphuc/notes" target="_blank" class="thumbnail">
-
 				<img src="/asset/data/note.jpg">
-
-
 			</a>
 			<a class='btn btn-lg btn-info' href="https://www.facebook.com/conduonghanhphuc/notes" target="_blank">
 				<i class="fa fa-facebook">
-
 				</i>acebook: Notes
-
 			</a>
-
 		</p>
 
 	</div>
@@ -74,7 +56,7 @@
 
 	<div class="col-md-3 text-center">
 		<p>
-			<a href="#" class="thumbnail">
+			<a href="https://docs.google.com/spreadsheets/d/1v9VLjRWpDYW8vZth40B5BrasOrfsouqkMy8ZZfYC0_s/edit#gid=0" class="thumbnail">
 
 				<img src="/asset/data/weight.jpg">
 
@@ -102,15 +84,11 @@
 
 	<div class="col-md-3 text-center">
 		<p>
-			<a href="#" class="thumbnail">
-
+			<a href="https://docs.google.com/spreadsheets/d/1T8uqVMo2ggxBYrKTczJ7rILQCCu1MwgWqcjRGqUpmbA/edit?ts=56912db3#gid=1470449781" class="thumbnail">
 				<img src="/asset/data/money.png">
-
-
 			</a>
 			<a class='btn btn-lg btn-primary' href="https://docs.google.com/spreadsheets/d/1T8uqVMo2ggxBYrKTczJ7rILQCCu1MwgWqcjRGqUpmbA/edit?ts=56912db3#gid=1470449781" target="_blank">
 				<i class="fa fa-google">
-
 				</i> Thu chi theo tháng
 
 			</a>
