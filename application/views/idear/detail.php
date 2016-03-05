6994d92e0ff112175b788056524b897c0d732a3f
Vi Hoang Son <vihoangson@gmail.com>
2016-03-05 14:56:44 +0700

    commit

--- application/views/idear/detail.php
+++ application/views/idear/detail.php
@@ -15,5 +15,18 @@ $this->load->view('_includes/header',$data_header); ?>
 	}
 	?>
 	<a href="/idear/edit/<?= $rs->id; ?>"><i class="fa fa-pencil"></i></a> <a href="/idear/delete/<?= $rs->id; ?>"><i class="fa fa-trash"></i></a>
+
+
+<div class="fb-comments" data-href="http://<?= $_SERVER["HTTP_HOST"]; ?>/idear/detail/<?= $rs->id; ?>" data-width="100%" data-numposts="5"></div>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=1649988411892120";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <?php $this->load->view('_includes/footer');
 ?>
