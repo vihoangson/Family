43cbc968afc6c32cc0e02a55c153fc8047ad1f8e
Santo CBA <santo@cybridge.jp>
2016-04-14 18:12:17 +0900

    update

--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,11 @@
 <?php $this->load->view('_includes/header',["css"=>[base_url()."asset/css/login.css"]]); ?>
-<script src='https://www.google.com/recaptcha/api.js'></script>
+<?php
+    define("RECAPTCHA",false);
+    if(RECAPTCHA){
+        ?><script src='https://www.google.com/recaptcha/api.js'></script><?php
+    }
+?>
+
 <div class="container">
     <div class="row">
         <div class='col-md-3'></div>
@@ -25,8 +31,8 @@
                         <div class="form-group">
                             <input type="submit" class="btn btn-default btn-login-submit btn-block m-t-md" value="Login" />
                         </div>
-                        <span class='text-center'><a href="/resetting/request" class="text-sm">Forgot Password?</a></span>
-                        <div class="form-group">
+                        <span class='text-center hidden'><a href="/resetting/request" class="text-sm">Forgot Password?</a></span>
+                        <div class="form-group hidden">
                             <p class="text-center m-t-xs text-sm">Do not have an account?</p> 
                             <a href="/register/" class="btn btn-default btn-block m-t-md">Create an account</a>
                         </div>
@@ -36,9 +42,16 @@
                     if(defined("APP_ID")){
                         ?>
                         <p class="text-center"><a href="<?= $url_fb; ?>"><img style="max-width:100%" src="/asset/data/login_w_facebook.png"></a></p>
-                        <div class="g-recaptcha" data-sitekey="6LfycxoTAAAAALXMmet9_axitVj5lObK5BXC9x9r"></div>
                         <?php
                     } ?>
+                    <p class="text-center"><a href='/friends' class="btn btn-primary btn-lgn"><i class="fa fa-smile-o"></i> My Friends</a></p>
+                    <?php
+                    //recapcha
+                    if(RECAPTCHA){
+                        ?><div class="g-recaptcha" data-sitekey="6LfycxoTAAAAALXMmet9_axitVj5lObK5BXC9x9r"></div><?php
+                    }
+                    ?>
+
             </div>
         </div>
         <div class='col-md-3'></div>
