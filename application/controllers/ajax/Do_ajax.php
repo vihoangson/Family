fdde921efc2ee94d3069552c419ddf6bfc8d1d6d
Vi Hoang Son <vihoangson@gmail.com>
2016-04-27 21:00:50 +0700

    update

--- application/controllers/ajax/Do_ajax.php
+++ application/controllers/ajax/Do_ajax.php
@@ -6,7 +6,43 @@ class Do_ajax extends CI_Controller {
 	public function index()
 	{
 		?>
-			<div class="text-center"><img class='avatar_element' src='http://family.vihoangson.com/asset/uploads/icon_lick.png'></div>
+			<div class="text-center">
+				<img class='avatar_element' src='http://family.vihoangson.com/asset/uploads/icon_lick.png'>
+				<img class='avatar_element' src='/asset/data/icon/851560_551710611530737_79745517_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851565_401768539958711_290028601_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851568_396469623830477_2084168344_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851575_150916178429304_670813584_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851575_194382550685748_925626113_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851578_551710598197405_1346658353_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851582_150916201762635_1565638622_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851584_396468773830562_144785597_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851584_443281099111868_1573675643_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851585_490565371056908_1157825914_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851586_126362104215253_1651254063_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851586_209575229232979_1004007314_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851586_582402925104020_1853411551_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851588_209575132566322_766940191_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851595_460938580694657_59246824_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10173505_313426158818204_775161941_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10541012_456591364482960_1246532948_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10574701_520074084802755_1429693945_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10734286_481172855355137_520706700_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10734286_1578461445722639_1330279923_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10734312_383649311796508_969637020_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10935979_383649298463176_1866205660_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10935981_383649261796513_392257764_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/10935995_782436578507958_328330484_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/12330625_944849862231621_426325917_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/12624115_1523526111274707_2069916634_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/12683864_1523525987941386_1924921931_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851539_272698496237749_786804483_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851543_167788180085135_771369768_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851549_377001615779232_1579499571_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851555_167788120085141_1443830988_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851555_443281115778533_230573929_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851558_150916185095970_1363767498_n.png'>
+				<img class='avatar_element' src='/asset/data/icon/851559_150916168429305_714795320_n.png'>
+			</div>
 		<?php
 	}
 
