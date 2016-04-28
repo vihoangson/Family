faa4fb2311f3be29b1b11dcfd07e8babdc7ce269
Son Vi Hoang <hoang_son@lampart-vn.com>
2016-04-28 17:18:53 +0700

    commit

--- application/controllers/ajax/Console.php
+++ application/controllers/ajax/Console.php
@@ -16,6 +16,62 @@ class Console extends CI_Controller {
 	// ! Hình nhỏ hơn ko resize
 	// /ajax/console/resize_imf
 	//============ ============ ============  ============  ============  ============ 
+	public function resize_gif(){
+		//============ ============  ============  ============ 
+		//  Đường dẫn folder
+		$direct_resize_all        = FCPATH."asset/file_upload";
+		// Chiều rộng và chiều cao cần resize
+		// Lưu ý hình nhỏ hơn số này không được resize
+		$thumbWidth               =50;
+		$thumbHeight              =50;
+		//============ ============  ============  ============ 
+		$config['image_library']  = 'gd2';
+		$config['create_thumb']   = TRUE;
+		$config['thumb_marker']   = "thumb_";
+		$config['maintain_ratio'] = TRUE;
+		$config['width']          = $thumbWidth;
+		$config['height']         = $thumbHeight;
+		$this->load->library('image_lib', $config);
+		// Đổ tất cả các thư mục con vào mảng
+		$directory_list = $this->listdirs($direct_resize_all);
+		// Check file tại thư mục chỉ định
+		$directory_list[] = $direct_resize_all;
+
+		foreach ($directory_list as $key_direct => $value_direct) {
+			$path_resize = $value_direct."/";
+			$dir = scandir($path_resize);
+			foreach ($dir as $key => $value) {
+				if(preg_match("/\.(gif)$/", $value)){
+					$path_file_name = $path_resize.$value;
+					list($width, $height) = getimagesize($path_file_name);
+					if($thumbWidth < $width || $thumbHeight < $height)
+					{
+						$config['source_image']	= $path_file_name;
+						$this->image_lib->clear();
+						$this->image_lib->initialize($config);					
+						if($this->image_lib->resize()){
+							$result[] = ["status"=>"done"];
+						}else{
+							$result[] = ["status"=>"error","content"=>$this->image_lib->display_errors()];
+						}
+					}
+					else
+					{
+						$result[] = ["status"=>"Don't resize"];
+					}
+				}
+			}
+		}
+		// Xuất json kết quả
+		echo json_encode($result);
+	}
+
+	//============ ============ ============  ============  ============  ============ 
+	// Function resize_imf()
+	// Function này dùng để resize tất cả hình ảnh trong folder cả subfolder
+	// ! Hình nhỏ hơn ko resize
+	// /ajax/console/resize_imf
+	//============ ============ ============  ============  ============  ============ 
 	public function resize_imf(){
 		//============ ============  ============  ============ 
 		//  Đường dẫn folder
