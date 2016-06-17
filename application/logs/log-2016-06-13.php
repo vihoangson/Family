df1fcf975909bc296808a3600be92557c00a8b8f
Son Vi Hoang <hoang_son@lampart-vn.com>
2016-06-17 10:50:58 +0700

    commit

---
+++ application/logs/log-2016-06-13.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-06-13 10:58:56 --> Severity: Error --> Class 'Entity\Kyniem' not found D:\xampp\htdocs\vhosts\family\application\controllers\Homepage.php 71
+ERROR - 2016-06-13 11:01:29 --> Severity: Error --> Class 'Entity\kyniem' not found D:\xampp\htdocs\vhosts\family\application\controllers\Homepage.php 71
