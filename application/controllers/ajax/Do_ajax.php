ecc70b23c6bac29314191a0d024af152e64c5e96
Son Vi Hoang <hoang_son@lampart-vn.com>
2016-06-23 13:41:03 +0700

    Add path backup

--- application/controllers/ajax/Do_ajax.php
+++ application/controllers/ajax/Do_ajax.php
@@ -13,9 +13,12 @@ class Do_ajax extends CI_Controller {
 	public function download_backup(){
 		ini_set('memory_limit', '228M');
 		$this->load->library('zip');
+		$path = FCPATH."asset/images";
+		$this->zip->read_dir($path);
 		$path = FCPATH."asset/uploads";
 		$this->zip->read_dir($path);
-
+		$path = APPPATH."models/db";
+		$this->zip->read_dir($path);
 
 		$file_name = "backup_file/".date("Y_m_d__H_i_s")."_backup_folder_asset.zip";
 		if($this->zip->archive(FCPATH.$file_name)==true){
