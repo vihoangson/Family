96c4656e095ad8ed43965482c10f672a32156de8
Son Vi Hoang <hoang_son@lampart-vn.com>
2016-06-23 11:49:14 +0700

    commit

--- application/controllers/ajax/Do_ajax.php
+++ application/controllers/ajax/Do_ajax.php
@@ -13,7 +13,7 @@ class Do_ajax extends CI_Controller {
 	public function download_backup(){
 		ini_set('memory_limit', '228M');
 		$this->load->library('zip');
-		$path = FCPATH."asset";
+		$path = FCPATH."asset/uploads";
 		$this->zip->read_dir($path);
 
 		$file_name = "backup_file/".date("Y_m_d__H_i_s")."_backup_folder_asset.zip";
