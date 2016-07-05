aee52c0edbf243e019526283a5953af37fdb8ff4
Son Vi Hoang <hoang_son@lampart-vn.com>
2016-07-05 08:25:24 +0700

    Fix commit

--- application/views/_includes/navbar.php
+++ application/views/_includes/navbar.php
@@ -3,6 +3,9 @@
 	<div class="navbar-header">
 		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
 			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
 		</button>
 		<a class="navbar-brand" href="<?= base_url(); ?>"><img style="margin-top:-6px;display:inline;" src="/favicon.ico"> My Family</a>
 	</div>
