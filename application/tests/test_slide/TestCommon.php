3446410fa6810a462d53a8eb547b1ad278b0199f
Vi Hoang Son <vihoangson@gmail.com>
2016-12-12 17:45:17 +0700

    commit

---
+++ application/tests/test_slide/TestCommon.php
@@ -0,0 +1,17 @@
+<?php
+require_once(__DIR__.'/../CITestCase.php');
+class TestCommon extends CITestCase {
+
+    /**
+     * Kiểm tra tồn tại folder
+     *
+     * @return void
+     * @author Hoang Son
+     * @create 20160610155500
+     **/
+    public function test_check_table_exists(){
+        $this->assertTrue(is_dir(FCPATH), "Không có folder:".FCPATH);
+    }
+
+
+}
