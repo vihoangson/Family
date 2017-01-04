34080f89a0ccbea0ad3471e515ea07d43d17d318
Vi Hoang Son <vihoangson@gmail.com>
2017-01-04 16:44:11 +0700

    commit

---
+++ blog/wp-config.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('WP_CACHE', true); //Added by WP-Cache Manager
+define( 'WPCACHEHOME', '/var/www/vhosts/family.vihoangson.com/blog/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
+define('DB_NAME', 'blog_family');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'rbmf5uBm5eZ9');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '/ha`h^t1EanHRRjA?07tck8-4 (Fdmiiw;)8r(/Jc*/gK%GN3+>j- i.3{_.<wRv');
+define('SECURE_AUTH_KEY',  '[=cGbPylkqPXe/U_WzSN6CLxX=1BAwz~5wS#$UbwJ}Vjj<]J/z#hQ)%*~tz/2ldJ');
+define('LOGGED_IN_KEY',    'A3xz3C+[a+6g9+*F,`%uOJ5*!bSz^}.V4BFg9ZPTb}CcKB4G&k4,546rPF~*16Ze');
+define('NONCE_KEY',        'W3P.C7IcQFJbgQh~*$kGU_za!sFu3L*kJ`,w8J_eDme7jp7HkRd3/3ylWm-Z*0tX');
+define('AUTH_SALT',        'u2BY>JMRKi+B~EDm$(Y+pB_<4wu4 ^^x+604(RtI`Gc-@-O9LvVsr-pxpTlt=tFY');
+define('SECURE_AUTH_SALT', '.z}I/*j@|-2$a6Hv>hMskpmC5vc>S7)sGo|D{+vY]CW!guY%-=pTfL3A&$}mABv3');
+define('LOGGED_IN_SALT',   '{a<2+5/5##g{~*o%*!dZZ.JE2P^5.#!R2L9doCH0^qcoeF((O+b!DXMf-=[QOF9X');
+define('NONCE_SALT',       'nwklwoH|1HpMGf)d(4g]})wS6``d)(?6#@A)/9yb*Q9^BQ~uWR!;(sl;*IQ{^27+');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'family_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+
+require_once(ABSPATH . 'wp-settings.php');
---
+++ blog/wp-content/cache/wp-cache-64db71cc8494b229e27fd3a67a12a2b6.php
@@ -0,0 +1,208 @@
+<?php die(); ?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="en-US">
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html lang="en-US">
+<!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Blog of Family | Just another WordPress site</title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="http://family.vihoangson.com/blog/xmlrpc.php">
+	<!--[if lt IE 9]>
+	<script src="http://family.vihoangson.com/blog/wp-content/themes/twentyfourteen/js/html5.js"></script>
+	<![endif]-->
+	<link rel="alternate" type="application/rss+xml" title="Blog of Family &raquo; Feed" href="http://family.vihoangson.com/blog/feed/" />
+<link rel="alternate" type="application/rss+xml" title="Blog of Family &raquo; Comments Feed" href="http://family.vihoangson.com/blog/comments/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":"http:\/\/family.vihoangson.com\/blog\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.5.3"}};
+			!function(a,b,c){function d(a){var c,d,e,f=b.createElement("canvas"),g=f.getContext&&f.getContext("2d"),h=String.fromCharCode;if(!g||!g.fillText)return!1;switch(g.textBaseline="top",g.font="600 32px Arial",a){case"flag":return g.fillText(h(55356,56806,55356,56826),0,0),f.toDataURL().length>3e3;case"diversity":return g.fillText(h(55356,57221),0,0),c=g.getImageData(16,16,1,1).data,d=c[0]+","+c[1]+","+c[2]+","+c[3],g.fillText(h(55356,57221,55356,57343),0,0),c=g.getImageData(16,16,1,1).data,e=c[0]+","+c[1]+","+c[2]+","+c[3],d!==e;case"simple":return g.fillText(h(55357,56835),0,0),0!==g.getImageData(16,16,1,1).data[0];case"unicode8":return g.fillText(h(55356,57135),0,0),0!==g.getImageData(16,16,1,1).data[0]}return!1}function e(a){var c=b.createElement("script");c.src=a,c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i;for(i=Array("simple","flag","unicode8","diversity"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='open-sans-css'  href='https://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.5.3' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://family.vihoangson.com/blog/wp-includes/css/dashicons.min.css?ver=4.5.3' type='text/css' media='all' />
+<link rel='stylesheet' id='admin-bar-css'  href='http://family.vihoangson.com/blog/wp-includes/css/admin-bar.min.css?ver=4.5.3' type='text/css' media='all' />
+<link rel='stylesheet' id='twentyfourteen-lato-css'  href='https://fonts.googleapis.com/css?family=Lato%3A300%2C400%2C700%2C900%2C300italic%2C400italic%2C700italic&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='http://family.vihoangson.com/blog/wp-content/themes/twentyfourteen/genericons/genericons.css?ver=3.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='twentyfourteen-style-css'  href='http://family.vihoangson.com/blog/wp-content/themes/twentyfourteen/style.css?ver=4.5.3' type='text/css' media='all' />
+<!--[if lt IE 9]>
+<link rel='stylesheet' id='twentyfourteen-ie-css'  href='http://family.vihoangson.com/blog/wp-content/themes/twentyfourteen/css/ie.css?ver=20131205' type='text/css' media='all' />
+<![endif]-->
+<script type='text/javascript' src='http://family.vihoangson.com/blog/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://family.vihoangson.com/blog/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<link rel='https://api.w.org/' href='http://family.vihoangson.com/blog/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://family.vihoangson.com/blog/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://family.vihoangson.com/blog/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.5.3" />
+		<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+		<style type="text/css" media="print">#wpadminbar { display:none; }</style>
+<style type="text/css" media="screen">
+	html { margin-top: 32px !important; }
+	* html body { margin-top: 32px !important; }
+	@media screen and ( max-width: 782px ) {
+		html { margin-top: 46px !important; }
+		* html body { margin-top: 46px !important; }
+	}
+</style>
+</head>
+
+<body class="home blog logged-in admin-bar no-customize-support masthead-fixed list-view full-width grid">
+<div id="page" class="hfeed site">
+	
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header-main">
+			<h1 class="site-title"><a href="http://family.vihoangson.com/blog/" rel="home">Blog of Family</a></h1>
+
+			<div class="search-toggle">
+				<a href="#search-container" class="screen-reader-text" aria-expanded="false" aria-controls="search-container">Search</a>
+			</div>
+
+			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+				<button class="menu-toggle">Primary Menu</button>
+				<a class="screen-reader-text skip-link" href="#content">Skip to content</a>
+				<div id="primary-menu" class="nav-menu"><ul><li class="page_item page-item-2"><a href="http://family.vihoangson.com/blog/sample-page/">Family</a></li></ul></div>
+			</nav>
+		</div>
+
+		<div id="search-container" class="search-box-wrapper hide">
+			<div class="search-box">
+				<form role="search" method="get" class="search-form" action="http://family.vihoangson.com/blog/">
+				<label>
+					<span class="screen-reader-text">Search for:</span>
+					<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" />
+				</label>
+				<input type="submit" class="search-submit" value="Search" />
+			</form>			</div>
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="main" class="site-main">
+
+<div id="main-content" class="main-content">
+
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+		
+<article id="post-25" class="post-25 post type-post status-publish format-standard hentry category-uncategorized">
+	
+	<header class="entry-header">
+		<h1 class="entry-title"><a href="http://family.vihoangson.com/blog/2016/08/14/chuyen-nha/" rel="bookmark">Chuyển nhà</a></h1>
+		<div class="entry-meta">
+			<span class="entry-date"><a href="http://family.vihoangson.com/blog/2016/08/14/chuyen-nha/" rel="bookmark"><time class="entry-date" datetime="2016-08-14T04:52:18+00:00">August 14, 2016</time></a></span> <span class="byline"><span class="author vcard"><a class="url fn n" href="http://family.vihoangson.com/blog/author/admin/" rel="author">admin</a></span></span>			<span class="comments-link"><a href="http://family.vihoangson.com/blog/2016/08/14/chuyen-nha/#respond">Leave a comment</a></span>
+			<span class="edit-link"><a class="post-edit-link" href="http://family.vihoangson.com/blog/wp-admin/post.php?post=25&#038;action=edit">Edit</a></span>		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+		<div class="entry-content">
+		<p>Bố Sơn đã dọn gần xong đồ đạc sang nhà mới, để chuẩn bị đón em Kem về nè.</p>
+<p><img class="aligncenter wp-image-26 size-full" src="http://family.vihoangson.com/blog/wp-content/uploads/2016/08/2016-08-14-11_48_33-1.png" alt="2016-08-14 11_48_33" width="529" height="408" srcset="http://family.vihoangson.com/blog/wp-content/uploads/2016/08/2016-08-14-11_48_33-1.png 529w, http://family.vihoangson.com/blog/wp-content/uploads/2016/08/2016-08-14-11_48_33-1-300x231.png 300w" sizes="(max-width: 529px) 100vw, 529px" /></p>
+	</div><!-- .entry-content -->
+	
+	</article><!-- #post-## -->
+
+<article id="post-16" class="post-16 post type-post status-publish format-standard hentry category-uncategorized">
+	
+	<header class="entry-header">
+		<h1 class="entry-title"><a href="http://family.vihoangson.com/blog/2016/08/06/hi-em-kem/" rel="bookmark">Chào con gái</a></h1>
+		<div class="entry-meta">
+			<span class="entry-date"><a href="http://family.vihoangson.com/blog/2016/08/06/hi-em-kem/" rel="bookmark"><time class="entry-date" datetime="2016-08-06T04:16:42+00:00">August 6, 2016</time></a></span> <span class="byline"><span class="author vcard"><a class="url fn n" href="http://family.vihoangson.com/blog/author/admin/" rel="author">admin</a></span></span>			<span class="comments-link"><a href="http://family.vihoangson.com/blog/2016/08/06/hi-em-kem/#respond">Leave a comment</a></span>
+			<span class="edit-link"><a class="post-edit-link" href="http://family.vihoangson.com/blog/wp-admin/post.php?post=16&#038;action=edit">Edit</a></span>		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+		<div class="entry-content">
+		<p>Bố thương con gái lắm nha ^^</p>
+<p><img class="size-medium wp-image-5 aligncenter" src="http://family.vihoangson.com/blog/wp-content/uploads/2016/08/IMG_1523-COLLAGE-300x300.jpg" alt="So cute" width="300" height="300" srcset="http://family.vihoangson.com/blog/wp-content/uploads/2016/08/IMG_1523-COLLAGE-300x300.jpg 300w, http://family.vihoangson.com/blog/wp-content/uploads/2016/08/IMG_1523-COLLAGE-150x150.jpg 150w, http://family.vihoangson.com/blog/wp-content/uploads/2016/08/IMG_1523-COLLAGE-768x768.jpg 768w, http://family.vihoangson.com/blog/wp-content/uploads/2016/08/IMG_1523-COLLAGE-1024x1024.jpg 1024w, http://family.vihoangson.com/blog/wp-content/uploads/2016/08/IMG_1523-COLLAGE-1200x1200.jpg 1200w" sizes="(max-width: 300px) 100vw, 300px" /></p>
+	</div><!-- .entry-content -->
+	
+	</article><!-- #post-## -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	</div><!-- #main-content -->
+
+<div id="secondary">
+		<h2 class="site-description">Just another WordPress site</h2>
+	
+	
+	</div><!-- #secondary -->
+
+		</div><!-- #main -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			
+			<div class="site-info">
+								<a href="https://wordpress.org/">Proudly powered by WordPress</a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<script type='text/javascript' src='http://family.vihoangson.com/blog/wp-includes/js/admin-bar.min.js?ver=4.5.3'></script>
+<script type='text/javascript' src='http://family.vihoangson.com/blog/wp-content/themes/twentyfourteen/js/functions.js?ver=20150315'></script>
+<script type='text/javascript' src='http://family.vihoangson.com/blog/wp-includes/js/wp-embed.min.js?ver=4.5.3'></script>
+	<script type="text/javascript">
+		(function() {
+			var request, b = document.body, c = 'className', cs = 'customize-support', rcs = new RegExp('(^|\\s+)(no-)?'+cs+'(\\s+|$)');
+
+			request = true;
+
+			b[c] = b[c].replace( rcs, ' ' );
+			b[c] += ( window.postMessage && request ? ' ' : ' no-' ) + cs;
+		}());
+	</script>
+			<div id="wpadminbar" class="nojq nojs">
+							<a class="screen-reader-shortcut" href="#wp-toolbar" tabindex="1">Skip to toolbar</a>
+						<div class="quicklinks" id="wp-toolbar" role="navigation" aria-label="Toolbar" tabindex="0">
+				<ul id="wp-admin-bar-root-default" class="ab-top-menu">
+		<li id="wp-admin-bar-wp-logo" class="menupop"><a class="ab-item"  aria-haspopup="true" href="http://family.vihoangson.com/blog/wp-admin/about.php"><span class="ab-icon"></span><span class="screen-reader-text">About WordPress</span></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-wp-logo-default" class="ab-submenu">
+		<li id="wp-admin-bar-about"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/about.php">About WordPress</a>		</li></ul><ul id="wp-admin-bar-wp-logo-external" class="ab-sub-secondary ab-submenu">
+		<li id="wp-admin-bar-wporg"><a class="ab-item"  href="https://wordpress.org/">WordPress.org</a>		</li>
+		<li id="wp-admin-bar-documentation"><a class="ab-item"  href="https://codex.wordpress.org/">Documentation</a>		</li>
+		<li id="wp-admin-bar-support-forums"><a class="ab-item"  href="https://wordpress.org/support/">Support Forums</a>		</li>
+		<li id="wp-admin-bar-feedback"><a class="ab-item"  href="https://wordpress.org/support/forum/requests-and-feedback">Feedback</a>		</li></ul></div>		</li>
+		<li id="wp-admin-bar-site-name" class="menupop"><a class="ab-item"  aria-haspopup="true" href="http://family.vihoangson.com/blog/wp-admin/">Blog of Family</a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-site-name-default" class="ab-submenu">
+		<li id="wp-admin-bar-dashboard"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/">Dashboard</a>		</li></ul><ul id="wp-admin-bar-appearance" class="ab-submenu">
+		<li id="wp-admin-bar-themes"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/themes.php">Themes</a>		</li>
+		<li id="wp-admin-bar-widgets"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/widgets.php">Widgets</a>		</li>
+		<li id="wp-admin-bar-menus"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/nav-menus.php">Menus</a>		</li>
+		<li id="wp-admin-bar-background" class="hide-if-customize"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/themes.php?page=custom-background">Background</a>		</li>
+		<li id="wp-admin-bar-header" class="hide-if-customize"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/themes.php?page=custom-header">Header</a>		</li></ul></div>		</li>
+		<li id="wp-admin-bar-customize" class="hide-if-no-customize"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/customize.php?url=http%3A%2F%2Ffamily.vihoangson.com%2Fblog%2F">Customize</a>		</li>
+		<li id="wp-admin-bar-updates"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/update-core.php" title="2 Plugin Updates, 3 Theme Updates"><span class="ab-icon"></span><span class="ab-label">5</span><span class="screen-reader-text">2 Plugin Updates, 3 Theme Updates</span></a>		</li>
+		<li id="wp-admin-bar-comments"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/edit-comments.php"><span class="ab-icon"></span><span id="ab-awaiting-mod" class="ab-label awaiting-mod pending-count count-0" aria-hidden="true">0</span><span class="screen-reader-text">0 comments awaiting moderation</span></a>		</li>
+		<li id="wp-admin-bar-new-content" class="menupop"><a class="ab-item"  aria-haspopup="true" href="http://family.vihoangson.com/blog/wp-admin/post-new.php"><span class="ab-icon"></span><span class="ab-label">New</span></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-new-content-default" class="ab-submenu">
+		<li id="wp-admin-bar-new-post"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/post-new.php">Post</a>		</li>
+		<li id="wp-admin-bar-new-media"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/media-new.php">Media</a>		</li>
+		<li id="wp-admin-bar-new-page"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/post-new.php?post_type=page">Page</a>		</li>
+		<li id="wp-admin-bar-new-user"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/user-new.php">User</a>		</li></ul></div>		</li></ul><ul id="wp-admin-bar-top-secondary" class="ab-top-secondary ab-top-menu">
+		<li id="wp-admin-bar-search" class="admin-bar-search"><div class="ab-item ab-empty-item" tabindex="-1"><form action="http://family.vihoangson.com/blog/" method="get" id="adminbarsearch"><input class="adminbar-input" name="s" id="adminbar-search" type="text" value="" maxlength="150" /><label for="adminbar-search" class="screen-reader-text">Search</label><input type="submit" class="adminbar-button" value="Search"/></form></div>		</li>
+		<li id="wp-admin-bar-my-account" class="menupop with-avatar"><a class="ab-item"  aria-haspopup="true" href="http://family.vihoangson.com/blog/wp-admin/profile.php">Howdy, admin<img alt='' src='http://1.gravatar.com/avatar/1414a3fc3f793cbee48a9f903e228095?s=26&#038;d=mm&#038;r=g' srcset='http://1.gravatar.com/avatar/1414a3fc3f793cbee48a9f903e228095?s=52&amp;d=mm&amp;r=g 2x' class='avatar avatar-26 photo' height='26' width='26' /></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-user-actions" class="ab-submenu">
+		<li id="wp-admin-bar-user-info"><a class="ab-item" tabindex="-1" href="http://family.vihoangson.com/blog/wp-admin/profile.php"><img alt='' src='http://1.gravatar.com/avatar/1414a3fc3f793cbee48a9f903e228095?s=64&#038;d=mm&#038;r=g' srcset='http://1.gravatar.com/avatar/1414a3fc3f793cbee48a9f903e228095?s=128&amp;d=mm&amp;r=g 2x' class='avatar avatar-64 photo' height='64' width='64' /><span class='display-name'>admin</span></a>		</li>
+		<li id="wp-admin-bar-edit-profile"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-admin/profile.php">Edit My Profile</a>		</li>
+		<li id="wp-admin-bar-logout"><a class="ab-item"  href="http://family.vihoangson.com/blog/wp-login.php?action=logout&#038;_wpnonce=9b3c3d1095">Log Out</a>		</li></ul></div>		</li></ul>			</div>
+						<a class="screen-reader-shortcut" href="http://family.vihoangson.com/blog/wp-login.php?action=logout&#038;_wpnonce=9b3c3d1095">Log Out</a>
+					</div>
+
+		</body>
+</html>
+<!-- Dynamic page generated in 0.068 seconds. -->
+<!-- Cached page generated by WP-Super-Cache on 2016-12-29 08:13:37 -->
