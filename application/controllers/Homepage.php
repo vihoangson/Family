d7df21c6cefb950f512c113e80bfa0dda9e58ce0
Vi Hoang Son <vihoangson@gmail.com>
2017-01-10 14:00:03 +0700

    fix

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -6,6 +6,7 @@ class Homepage extends MY_Controller {
 
 	public function __construct(){
 		parent::__construct();
+		$this->load->config('family/config');
 		$this->load->model('kyniem');
 		$this->load->library('image_lib');
 		$this->load->library('facebook');
